cda5d453aba9591c88e70ad94ddc1760da77ddb6
Rahul S Kumar <63488132+RahulSKumar-1998@users.noreply.github.com>
2026-02-17 18:29:56 +0530

    Setup: Add weak_crypto.py

---
+++ weak_crypto.py
@@ -0,0 +1,13 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+
+def encrypt_data(data, key):
+    # VULNERABILITY: ECB mode is insecure
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+    encryptor = cipher.encryptor()
+    return encryptor.update(data) + encryptor.finalize()
+
+key = os.urandom(32)
+data = b"Secret Message"
+encrypted = encrypt_data(data, key)
+print(encrypted)
